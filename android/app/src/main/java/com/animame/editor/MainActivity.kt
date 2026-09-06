b5f1465fe57507daca0ca8e91da4351288345ef5
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-06 08:38:59 +0100

    Add Android editor activity

---
+++ android/app/src/main/java/com/animame/editor/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.animame.editor
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.pm.ActivityInfo
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        setContentView(EditorView(this))
+    }
+}
