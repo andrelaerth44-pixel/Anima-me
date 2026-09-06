e43131f3f82a398553a298fd0637004f13b162e2
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-06 09:52:38 +0100

    feat: switch Android editor to functional V4 surface

--- android/app/src/main/java/com/animame/editor/MainActivity.kt
+++ android/app/src/main/java/com/animame/editor/MainActivity.kt
@@ -5,9 +5,21 @@ import android.os.Bundle
 import android.content.pm.ActivityInfo
 
 class MainActivity : Activity() {
+    private lateinit var editor: AnimationEditorViewV4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        setContentView(AnimationEditorViewV3(this))
+        editor = AnimationEditorViewV4(this)
+        setContentView(editor)
+    }
+
+    override fun onBackPressed() {
+        if (::editor.isInitialized) {
+            // Keep the editor state alive; the system can still finish the activity normally.
+            super.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
